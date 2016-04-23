7ae448ab0c712c5c2a63112070d049ad5516cf92
Dima Petruk <bavaria95@gmail.com>
2016-04-23 14:38:25 +0200

    user mapping

---
+++ database_helper.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+import binascii
+import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///aircheck.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = 'User'
+
+    id = db.Column(db.Integer, primary_key=True, unique=True, index=True, autoincrement=True)
+    email = db.Column(db.String, unique=True)
+    password = db.Column(db.String, nullable=False)
+
+if __name__ == "__main__":
+    db.create_all()    
