f72e2c70836c898cddc6d9213a152631c1215ec6
Dima Petruk <bavaria95@gmail.com>
2016-04-23 16:33:20 +0200

    new architecture

--- helper.py
+++ helper.py
@@ -1,12 +1,29 @@
 import os
 import binascii
-import datetime
-import re
+
+class Storage():
+    def __init__(self):
+        self.d = {}
+
+    def add_user(self, token, email):
+        if token in self.d:
+            raise 'Token is used.'
+
+        self.d[token] = email
+
+    def remove_user(self, token):
+        self.d.pop(token, None)
+
+    def get_user_email(self, token):
+        return self.d.get(token)
+
+    def is_token_presented(self, token):
+        return token in self.d
+
+    def get_all_storage(self):
+        return self.d
+
 
 def generate_random_token():
-	token_length = 36
-	return binascii.hexlify(os.urandom(token_length))
-
-
-def email_validator(email):
-	return re.match(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", email) is not None
+    token_length = 36
+    return binascii.hexlify(os.urandom(token_length))
