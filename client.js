9bb60803851fc3dab0ef35a40962aac8fa90c3a8
Dima Petruk <bavaria95@gmail.com>
2016-04-24 01:56:51 +0200

    leaflet library

--- client.js
+++ client.js
@@ -215,6 +215,8 @@ activate_home = function() {
     document.getElementById('browse-view').style = "display: none;";
     highlight_label('home');
 
+    create_map();
+
     display_user_symptoms();
     get_list_of_areas();
 }
@@ -398,4 +400,41 @@ show_profile = function(data) {
     document.getElementById('search-content').style = "display: none;";
     document.getElementById('profile-unfam').style = "display: block;";
     // fill_user_info_fields('browse', data);
+}
+
+create_map = function() {
+    var mymap = L.map('mapid').setView([51.505, -0.09], 13);
+
+        L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpandmbXliNDBjZWd2M2x6bDk3c2ZtOTkifQ._QA7i5Mpkd_m30IGElHziw', {
+            maxZoom: 18,
+            id: 'mapbox.streets'
+        }).addTo(mymap);
+
+
+        L.marker([51.5, -0.09]).addTo(mymap)
+            .bindPopup("<b>Hello world!</b><br />I am a popup.").openPopup();
+
+        L.circle([51.508, -0.11], 500, {
+            color: 'red',
+            fillColor: '#f03',
+            fillOpacity: 0.5
+        }).addTo(mymap).bindPopup("I am a circle.");
+
+        L.polygon([
+            [51.509, -0.08],
+            [51.503, -0.06],
+            [51.51, -0.047]
+        ]).addTo(mymap).bindPopup("I am a polygon.");
+
+
+        var popup = L.popup();
+
+        function onMapClick(e) {
+            popup
+                .setLatLng(e.latlng)
+                .setContent("You clicked the map at " + e.latlng.toString())
+                .openOn(mymap);
+        }
+
+        mymap.on('click', onMapClick);
 }
