1fc18f2271756062253a094e69461ce94bb03b87
mkloczko <mateusz.p.kloczko@gmail.com>
2016-04-24 13:08:51 +0200

    Added a working example of OSM, CO2 overlay, and clusters.

---
+++ gibs_example1.js
@@ -0,0 +1,155 @@
+/**
+ * GIBS Web Examples
+ *
+ * Copyright 2013 - 2014 United States Government as represented by the
+ * Administrator of the National Aeronautics and Space Administration.
+ * All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+var toggleCO2;
+
+window.onload = function() {
+
+    var source = new ol.source.MapQuest({layer: 'sat'})
+
+    var osm_source = new ol.source.OSM();
+
+    var source2 = new ol.source.WMTS({
+        url: "//map1{a-c}.vis.earthdata.nasa.gov/wmts-geo/",
+        layer: "SMAP_L4_Mean_Gross_Primary_Productivity",
+        time: "2015-09-17",
+        matrixSet: "EPSG4326_2km",
+        format: "image/png",
+
+       // service: "OGC WMTS",
+
+
+        tileGrid: new ol.tilegrid.WMTS({
+            origin: [-180, 90],
+            resolutions: [
+                0.5625,
+                0.28125,
+                0.140625,
+                0.0703125,
+                0.03515625,
+                0.017578125
+                // 0.0087890625,
+                // 0.00439453125,
+                // 0.002197265625
+            ],
+            matrixIds: [0, 1, 2, 3, 4, 5],
+            tileSize: 512
+        })
+    });
+
+    var features = new Array(3);
+
+    function toFeature(lat,lon){
+        return new ol.Feature(new ol.geom.Point([lon,lat]));
+    }
+    features[0] = toFeature(50.061389, 19.938333)
+
+    features[1] = toFeature(58.4, 15.616667);
+    features[2] = toFeature(-27.166667, -109.416667);
+    
+
+    console.log(ol.proj.transform([50.061389, 19.938333], 'EPSG:4326', 'EPSG:3857'));
+    var sourceVec = new ol.source.Vector({
+        features: features
+    });
+
+    var clusterSource = new ol.source.Cluster({
+        distance: 40,
+        source: sourceVec
+    });
+
+
+    
+    var styleCache = {};
+
+    var clusters = new ol.layer.Vector({
+        source: clusterSource,
+        style: function(feature) {
+            var size = feature.get('features').length;
+            console.log("Size",size);
+            var style = styleCache[size];
+            if (!style) {
+                style = new ol.style.Style({
+                    image: new ol.style.Circle({
+                        radius: 10,
+                        stroke: new ol.style.Stroke({
+                            color: '#fff'
+                        }),
+                        fill: new ol.style.Fill({
+                            color: '#3399CC'
+                        })
+                    }),
+                    text: new ol.style.Text({
+                        text: size.toString(),
+                        fill: new ol.style.Fill({
+                            color: '#fff'
+                        })
+                    })
+                });
+                styleCache[size] = style;
+            }
+            return style;
+        }
+    });
+    
+    var layer = new ol.layer.Tile({
+        source: source
+    });
+
+    var layer2 = new ol.layer.Tile({
+        source: source2,
+        opacity: 0.55
+    });
+
+
+
+    var layer3 = new ol.layer.Tile({
+        source: osm_source,
+        opacity: 1
+    });
+
+
+    var map = new ol.Map({
+        layers: [layer3, layer2, clusters],
+        renderer: ['canvas','dom'],
+        target: 'map',
+        view: new ol.View({
+            projection: ol.proj.get("EPSG:4326"),
+            extent: [-180, -90, 180, 90],
+            center: [0, 0],
+            zoom: 2
+        })
+    });
+
+    toggleCO2 = function(){
+        layer2.setVisible(!layer2.getVisible());
+    }
+
+
+    map.getView().on('change:resolution', function(){
+        var zoom = map.getView().getZoom();
+        // console.log("whee", map.getView().getZoom())
+
+        var zoom_capped = Math.min(14,Math.max(zoom,8)) - 8;
+        var opacity_val = (1-((zoom_capped)/6.0)) * 0.55;
+
+        layer2.setOpacity(opacity_val);
+    })
+};
